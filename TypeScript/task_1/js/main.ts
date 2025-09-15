837ecf96fc4680ad1b2b86c376254fd469714dcf
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-09-14 22:03:26 -0400

    task 4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -31,3 +31,28 @@ console.log(printTeacher("John", "Doe"));
 
 
 console.log(director1);
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass("Alice", "Smith");
+console.log(student.displayName());
+console.log(student.workOnHomework());
