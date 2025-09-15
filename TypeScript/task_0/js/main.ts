7782b55f3e48252b2609dade3cdb307dec5f45f4
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-09-14 20:19:40 -0400

    task 0

---
+++ TypeScript/task_0/js/main.ts
@@ -0,0 +1,42 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: "Ezra",
+  lastName: "Hart",
+  age: 30,
+  location: "Romania"
+};
+
+const student2: Student = {
+  firstName: "Archer",
+  lastName: "Vaughn",
+  age: 36,
+  location: "London"
+};
+
+const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement("table");
+const headerRow = table.insertRow();
+
+const header1 = headerRow.insertCell();
+header1.textContent = "First Name";
+
+const header2 = headerRow.insertCell();
+header2.textContent = "Location";
+
+studentsList.forEach((student) => {
+  const row = table.insertRow();
+  const firstNameCell = row.insertCell();
+  const locationCell = row.insertCell();
+
+  firstNameCell.textContent = student.firstName;
+  locationCell.textContent = student.location;
+});
+
+document.body.appendChild(table);
