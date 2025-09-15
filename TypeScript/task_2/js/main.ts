1dd2edf08fec1ca221ade645a65c9ff107b58ca9
Paola-cmyk <paonicolegonzalez@gmail.com>
2025-09-14 23:07:54 -0400

    task 7

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -61,3 +61,16 @@ function executeWork(employee: Director | Teacher): string {
 console.log(executeWork(createEmployee(200)));
 console.log(executeWork(createEmployee(1000)));
 console.log(createEmployee('$500'));
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  } else {
+    return "Teaching History";
+  }
+}
+
+console.log(teachClass("Math"));
+console.log(teachClass("History"));
